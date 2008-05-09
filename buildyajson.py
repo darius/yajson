e11a950d1e37240d5111555126924890e134db46
darius <darius@66-215-9-238.dhcp.gldl.ca.charter.com>
2008-05-09 09:51:35 -0700

    Fix the leftover description from the other sourcefile I copied to make this one. And the version number, too. Still not quite right, I suspect.

--- buildyajson.py
+++ buildyajson.py
@@ -3,7 +3,7 @@ from distutils.core import setup, Extension
 module1 = Extension('yajson',
 		    sources = ['yajson.c'])
 
-setup (name = 'cmyjson',
-       version = '1.0',
-       description = 'Find nearest points in a tag space.',
+setup (name = 'yajson',
+       version = '0.1',
+       description = 'JSON parser builder from an LL(1) grammar.',
        ext_modules = [module1])
